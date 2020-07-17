f0bf40e3457fd599dbe929718abe90498926f71d
JOHN_MARK_MADISON <HeavyMetalCookies@GMail.com>
2020-07-17 18:52:52 -0400

    [fix]

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ const HN5_Wri_sob=function( sob ){ "use strict"
                 
                     var pam =(  any_obj[ str_key ] );
                     for( k_v of Object.entries( pam ) ){
-                        str_val+=( k_v[0] ":" k_v[1] );
+                        str_val+=( k_v[0]+ ":" +k_v[1] );
                         str_val+=( "\n" );
                     };;
                 };;
